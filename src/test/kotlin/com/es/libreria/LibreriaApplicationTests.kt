8e1f1a681e471403c77b83637c40ec2b95e817b2
Lainezz <diegolinaresmojacar@gmail.com>
2024-11-07 17:47:17 +0100

    first commit

---
+++ src/test/kotlin/com/es/libreria/LibreriaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.es.libreria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibreriaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
